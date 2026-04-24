c3c2880da97a8424321f505d5bad983b03282be4
sudo-mac <sudo-mac@users.noreply.github.com>
2026-04-24 14:22:36 -0400

    fix

--- Pixel7-crDroid-A16.sh
+++ Pixel7-crDroid-A16.sh
@@ -6,12 +6,12 @@
 
 # ROM
 echo "Downloading crDroid12 (Android 16) ROM for Pixel 7..."
-wget -c --progress=bar --limit-rate=1g https://sourceforge.net/projects/crdroid/files/panther/12.x/crDroidAndroid-16.0-20260415-panther-v12.9.zip/download -O ROM.zip
+wget -qc --show-progress https://sourceforge.net/projects/crdroid/files/panther/12.x/crDroidAndroid-16.0-20260415-panther-v12.9.zip/download -O ROM.zip
 
 # Recovery
 echo "Downloading recovery..."
-wget -c --progress=bar --limit-rate=1g https://sourceforge.net/projects/crdroid/files/panther/12.x/recovery/20260415-vendor_boot.img/download -O vendor_boot.img
+wget -qc --progress=bar https://sourceforge.net/projects/crdroid/files/panther/12.x/recovery/20260415-vendor_boot.img/download -O vendor_boot.img
 
 # Gapps
 echo "Downloading Gapps for Android 16..."
-wget -c --progress=bar --limit-rate=1g "https://sourceforge.net/projects/nikgapps/files/Elite-Releases/306bobby/Android-16/16-Jul-2025/NikGapps-redbull-lite-arm64-16-20250716-signed.zip/download" -O Gapps.zip
+wget -qc --progress=bar "https://sourceforge.net/projects/nikgapps/files/Elite-Releases/306bobby/Android-16/16-Jul-2025/NikGapps-redbull-lite-arm64-16-20250716-signed.zip/download" -O Gapps.zip
--- download-apps.sh
+++ download-apps.sh
@@ -6,19 +6,19 @@
 
 # AirDroid Remote Support
 echo "Downloading Remote Support..."
-wget -c --progress=bar --limit-rate=1g https://dl.airdroid.com/RemoteSupport_1.1.4.0.apk -O Support.apk
+wget -qc --show-progress https://dl.airdroid.com/RemoteSupport_1.1.4.0.apk -O Support.apk
 
 # RootChecker
 echo "Downloading RootChecker..."
-wget -c --progress=bar --limit-rate=1g https://apkpure.com/root-checker/com.joeykrim.rootcheck/download -O RootChecker.apk
+wget -qc --show-progress https://apkpure.com/root-checker/com.joeykrim.rootcheck/download -O RootChecker.apk
 
 # RootExplorer
 echo "Downloading RootExplorer..."
-wget -c --progress=bar --limit-rate=1g https://d.apkpure.com/b/APK/com.speedsoftware.rootexplorer?version=latest -O RootExplorer.apk
+wget -qc --show-progress https://d.apkpure.com/b/APK/com.speedsoftware.rootexplorer?version=latest -O RootExplorer.apk
 
 # APKCombo
 echo "Downloading APKCombo..."
-wget -c --progress=bar --limit-rate=1g https://apkcombo.com/tr/apkcombo-installer/com.apkcombo.app/ -O APKCombo.apk
+wget -qc --show-progress https://apkcombo.com/tr/apkcombo-installer/com.apkcombo.app/ -O APKCombo.apk
 
 # HideMockLocation
 echo "Downloading HideMockLocation..."
@@ -26,35 +26,35 @@ curl -sL $(curl -s https://api.github.com/repos/auag0/HideMockLocation/releases/
 
 # GPSJoystick
 echo "Downloading GPSJoystick..."
-wget -c --progress=bar --limit-rate=1g https://downloads.theappninjas.com/gpsjoystick-unlocked-arm64-v8a.apk -O GPSJoystick.apk
+wget -qc --show-progress https://downloads.theappninjas.com/gpsjoystick-unlocked-arm64-v8a.apk -O GPSJoystick.apk
 
 # Lawnchair
 echo "Downloading Lawnchair..."
-wget -c --progress=bar --limit-rate=1g $(curl -sL https://api.github.com/repos/lawnchairlauncher/lawnchair/releases/latest | grep "browser_download_url.*\.apk" | cut -d '"' -f 4) -O Lawnchair.apk
+wget -qc --show-progress $(curl -sL https://api.github.com/repos/lawnchairlauncher/lawnchair/releases/latest | grep "browser_download_url.*\.apk" | cut -d '"' -f 4) -O Lawnchair.apk
 
 # PGsharp
 echo "Downloading PGsharp..."
-wget -c --progress=bar --limit-rate=1g https://www.pgsharp.com/dlpgsrt -O PGsharp.apk
+wget -qc --show-progress https://www.pgsharp.com/dlpgsrt -O PGsharp.apk
 
 # Pokemod
 echo "Downloading Pokemod..."
-wget -c --progress=bar --limit-rate=1g https://discovery.pokemod.dev/dl/pkmd/public -O Pokemod.apk
+wget -qc --show-progress https://discovery.pokemod.dev/dl/pkmd/public -O Pokemod.apk
 
 # PolygonX
 echo "Downloading PolygonX..."
-wget -c --progress=bar --limit-rate=1g "https://polygonx.dl.assets.evermorelabs.io/apk/$(curl -s https://polygonx.dl.assets.evermorelabs.io/apk/ | grep -O 'href="[^"]*\.apk"' | cut -d'"' -f2 | tail -1)" -O PolygonX.apk
+wget -qc --show-progress "https://polygonx.dl.assets.evermorelabs.io/apk/$(curl -s https://polygonx.dl.assets.evermorelabs.io/apk/ | grep -O 'href="[^"]*\.apk"' | cut -d'"' -f2 | tail -1)" -O PolygonX.apk
 # PGtools
 echo "Downloading PGtools..."
-wget -c --progress=bar --limit-rate=1g https://assets.pgtools.net/pgtools-5.6.15.apk -O PGtools.apk
+wget -qc --show-progress https://assets.pgtools.net/pgtools-5.6.15.apk -O PGtools.apk
 
 # Shungo
 echo "Downloading Shungo..."
-wget -c --progress=bar --limit-rate=1g https://shungo.app/shungo.apk -O Shungo.apk
+wget -qc --show-progress https://shungo.app/shungo.apk -O Shungo.apk
 
 # DataHub
 echo "Downloading DataHub..."
-wget -c --progress=bar --limit-rate=1g https://assets.pgtools.net/data-hub-2.3.0.apk -O DataHub.apk
+wget -qc --show-progress https://assets.pgtools.net/data-hub-2.3.0.apk -O DataHub.apk
 
 # AdAway
 echo "Downloading AdAway..."
-wget -c --progress=bar --limit-rate=1g $(curl -sL https://api.github.com/repos/AdAway/AdAway/releases/latest | grep "browser_download_url.*\.apk" | cut -d '"' -f 4) -O AdAway.apk
+wget -qc --show-progress $(curl -sL https://api.github.com/repos/AdAway/AdAway/releases/latest | grep "browser_download_url.*\.apk" | cut -d '"' -f 4) -O AdAway.apk
